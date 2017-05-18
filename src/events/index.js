2ae16d160dd90c88c7316a53c8d095b13e4e7ace
nemanjan00 <nemanjan00@gmail.com>
2017-05-18 17:03:34 +0200

    Events

---
+++ src/events/index.js
@@ -0,0 +1,45 @@
+var redis = require("redis");
+
+var util = require('util');
+var EventEmitter = require('events');
+
+module.exports = function(){
+	var queue = {
+		_client: undefined,
+
+		_handler: function(channel){
+			queue._client.blpop("tasks", 5, function(err, data){
+				if(data != null){
+					queue.prototype.emit(channel, data[1]);
+				}
+
+				queue._handler(channel);
+			});
+		},
+		on: function(eventName, listener){
+			queue._handler(eventName);
+			return queue.prototype.on(eventName, listener);
+		},
+		emit: function(eventName, data){
+			return new Promise(function(resolve, reject){
+				queue._client.rpush(eventName, data, function(err, data){
+					if(err !== null){
+						reject(err);
+					} else {
+						resolve(data);
+					}
+				});
+			});
+		}
+	}
+
+	EventEmitter.call(queue);
+
+	queue.prototype = {};
+	util.inherits(queue, EventEmitter);
+
+	queue._client = redis.createClient.apply(this, arguments);
+
+	return queue;
+}
+
